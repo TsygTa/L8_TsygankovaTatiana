e2caea4e18993134b4717a4471dfa7e94d54311b
TsygTa <670724@gmail.com>
2018-10-23 11:20:49 +0300

    Initial Commit

---
+++ L8_TsygankovaTatianaTests/L8_TsygankovaTatianaTests.swift
@@ -0,0 +1,34 @@
+//
+//  L8_TsygankovaTatianaTests.swift
+//  L8_TsygankovaTatianaTests
+//
+//  Created by Цыганкова Татьяна on 23.10.2018.
+//  Copyright © 2018 Цыганкова Татьяна. All rights reserved.
+//
+
+import XCTest
+@testable import L8_TsygankovaTatiana
+
+class L8_TsygankovaTatianaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
